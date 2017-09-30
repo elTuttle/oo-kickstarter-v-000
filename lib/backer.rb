b32cd79a0a5c0e50da18fc3742c676fa48438098
John Tuttle <thejwt@gmail.com>
2017-09-30 03:08:52 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,10 @@ class Backer
   end
 
   def back_project(project)
-    @backed_projects << project
+    if !backed_projects.include(project)
+      @backed_projects << project
+    end
     project.add_backer(self)
-
   end
 
 end
